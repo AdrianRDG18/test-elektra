3b5f2c91fd4f717e590c72ec764ddcc1e72324b5
AdrianRDG <adrian.rojas@tergar.org>
2025-08-04 15:38:20 -0600

    Update: add router-outlet for child routes

--- src/app/pages/pokemon-dashboard-page/pokemon-dashboard-page.ts
+++ src/app/pages/pokemon-dashboard-page/pokemon-dashboard-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar-component/navbar-component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-pokemon-dashboard-page',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, RouterOutlet],
   templateUrl: './pokemon-dashboard-page.html',
   styles: ``
 })
